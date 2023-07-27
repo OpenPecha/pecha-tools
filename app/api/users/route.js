c7955d3bfa1fe85d87a19456fa621b39d5dc63a8
Sonam Phuntsog <spsither@gmail.com>
2023-07-27 23:10:24 +0530

    fix(users): update user list

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,110 +1,143 @@
 import { NextResponse } from "next/server";
 const data = [
   // stt_cs
-  "lobsang",
-  "chemi",
-  // stt_cs_review
-  "phurbu",
-  // stt_cs_gb
+  "nganam",
   "tseten",
   "tsepakdorjee",
   "kelsang",
-  // stt_cs_gb_review
+  "dhonlhamo",
+  "choezin",
+  "choeyang ",
+  "sonampaldon",
+  "dekyidolma",
+  "karmatashi",
+  "lhamo",
+  "gelekchoephel",
+  "woeser",
+  "ngawangnorbu",
+  "yangchen",
+  "tashinamgyal",
+  "choying",
+  "palkar",
+  "tenpa",
+  "lutsokyi",
+  "choenga",
+  "dundultso",
+  "lekshey",
+  "kunsang",
+  "chemi",
+  "nordom",
+  "lowang",
+  "ngachung",
+  "fandey",
+  "talo",
+  "tashi",
+  // stt_cs_review
+  "phurbu",
   "tenchoeku",
-  //stt_tt_ga
+  "sherabchoekey",
+  "sopalama",
+  "penpatsering",
+  // stt_tt
   "kundol",
   "samp",
   "santen",
   "pandol",
   "kardor",
   "ringgyal",
-  //stt_tt_gb
   "dawala",
   "tamdin",
   "loge",
   "kinly",
   "choTsedup",
   "meydon",
-  //stt_tt_gc
   "tseyou",
   "thoesam",
   "pema",
   "choesang",
   "keschoki",
-  //stt_tt_gd
   "lhundup22",
   "rigdor22",
   "Tsultrim22",
   "Taga22",
   "Tsondue22",
-  //stt_tt_ge
-  "Yonten", 
+  "Tarab22",
+  "Yonten ",
   "ddolma",
-  "Jigmey", 
-  "Thardo", 
-  "Samten", 
-  "Norbu", 
-  "Tsewang", 
+  "Jigmey ",
+  "Thardo ",
+  "Samten ",
+  "Norbu ",
+  "Tsewang ",
   "Tinlaytse",
-  //stt_tt_gf
+  "Konga",
   "choephel",
   "dolma",
   "tsomo",
   "nyidon",
   "sangye",
   "kelsang",
-  //stt_tt_gg
   "Lsonam",
   "Lyarphel",
   "Ltsering",
   "Tnamgyal",
   "Knyimal",
-  //stt_tt_gh
   "GonpoZ",
   "KunchokZ",
   "ChoedakZ",
   "WoeserZ",
-  //stt_tt_ga_review
+  "GangpuZ",
+  "SonamZ",
+  //stt_tt_review
   "nyima",
-  //stt_tt_gb_review
   "jamlob",
-  //stt_tt_gc_review
   "kaschoedon",
-  //stt_tt_gd_review
-  "rangdol",
-  //stt_tt_ge_review
+  "ngodup",
   "celon",
-  //stt_tt_gf_review
   "Dharden",
-  //stt_tt_gg_review
   "karma",
-  //stt_tt_gh_review
   "DechenZ",
-  //stt_ns_ga
-  "TseringL",
+  //stt_ab
+  "choephel",
+  "dolma",
+  "tsomo",
+  "nyidon",
+  "sangye",
+  "kelsang",
+  "Tsondue",
+  //stt_ab_review
+  "Dharden",
+  "Yeshi",
+  //stt_ns
   "JampaL",
   "TsewangL",
   "PenpaL",
   "SherabL",
   "TashiL",
-  //stt_ns_gb
+  "NorzinL",
   "NTashi",
-  "NPhuntsok",
+  "NTsesang",
   "NTsewang",
   "NDorjee",
   "NWoesel",
   "NJigmey",
-  //stt_ns_gc
   "DJorden",
   "DTsundue",
   "DNagwang",
-  "DChungtak",
-  //stt_ns_ga_review
-  "NLhamo",
-  //stt_ns_gb_review
+  "TTashi",
+  "TLodoe",
+  "TChoeying",
+  "TDorjee",
+  "TSonam ",
+  "TStanzin",
+  "Tkardol",
+  "TTsemcak",
+  //stt_ns_review
+  "TashiN",
   "TNyendak",
-  //stt_ns_gc_review
-  "TDesel"
+  "Deditsering",
+  "LKunphel",
+  "Ddhundup"
 ];
 export async function GET() {
   return NextResponse.json({ data });
