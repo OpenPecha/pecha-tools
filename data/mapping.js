ef81973194175b5498b7b3c35f61f6eb55611d49
Sonam Phuntsog <spsither@gmail.com>
2023-07-31 16:01:37 +0530

    test(mapping): kun

--- data/mapping.js
+++ data/mapping.js
@@ -2,6 +2,7 @@ const mappingObj =
 {
     "spsither@gmail.com": { url: "work.pecha.tools/stt_cs_gb_review/?session=spsither@gmail.com", department: "stt" },
     "test@gmail.com": { url: "work.pecha.tools/stt_cs_gb_review/?session=test", department: "ocr" },
+    "tenkus47@gmail.com" : { url: "work.pecha.tools/stt_cs_gb_review/?session=spsither@gmail.com", department: "stt" },
 }
 
 export const mapping = new Map(Object.entries(mappingObj));
