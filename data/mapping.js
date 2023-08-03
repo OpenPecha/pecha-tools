c917319ff9b56c87384b8b3051396ade15ba0a75
Tashi Tsering <43548581+ta4tsering@users.noreply.github.com>
2023-08-03 14:45:23 +0530

    Update mapping.js

--- data/mapping.js
+++ data/mapping.js
@@ -1,7 +1,7 @@
 const mappingObj =
 {
     "spsither@gmail.com": { url: "work.pecha.tools/stt_cs_gb_review/?session=spsither@gmail.com", department: "stt" },
-    "test@gmail.com": { url: "work.pecha.tools/stt_cs_gb_review/?session=test", department: "ocr" },
+    "tashitsering@esukhia.org": { url: "work.pecha.tools/stt_cs_gb_review/?session=tashitsering@esukhia.org", department: "ocr" },
     "tenkus47@gmail.com" : { url: "work.pecha.tools/stt_cs_gb_review/?session=spsither@gmail.com", department: "stt" },
 }
 
