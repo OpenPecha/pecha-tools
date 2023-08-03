5585e98f66a288560eadfe295d28efb951145570
Tashi Tsering <43548581+ta4tsering@users.noreply.github.com>
2023-08-03 15:38:38 +0530

    Update mapping.js

--- data/mapping.js
+++ data/mapping.js
@@ -3,6 +3,18 @@ const mappingObj =
     "spsither@gmail.com": { url: "work.pecha.tools/stt_cs_gb_review/?session=spsither@gmail.com", department: "stt" },
     "tashitsering@esukhia.org": { url: "work.pecha.tools/glyph_annotation_ga/?session=tashitsering@esukhia.org", department: "ocr" },
     "tenkus47@gmail.com" : { url: "work.pecha.tools/stt_cs_gb_review/?session=spsither@gmail.com", department: "stt" },
+    "shugtso@monlam.ai" : { url: "work.pecha.tools/glyph_annotation_ga/?session=shugtso@monlam.ai", department: "ocr" },
+
+    "sangmotenz79@gmail.com" : { url: "work.pecha.tools/glyph_annotation_ga/?session=sangmotenz79@gmail.com", department: "ocr" },
+    "choezom2015@gmail.com" : { url: "work.pecha.tools/glyph_annotation_ga/?session=choezom2015@gmail.com", department: "ocr" },
+    "tsedhak123@gmail.com" : { url: "work.pecha.tools/glyph_annotation_ga/?session=tsedhak123@gmail.com", department: "ocr" },
+    "dakung132@gmail.com" : { url: "work.pecha.tools/glyph_annotation_ga/?session=dakung132@gmail.com", department: "ocr" },
+    
+    "yeshidolma5882@gmail.com" : { url: "work.pecha.tools/glyph_annotation_gb/?session=", department: "ocr" },
+    "tenphuns@gmail.com" : { url: "work.pecha.tools/glyph_annotation_gb/?session=tenphuns@gmail.com", department: "ocr" },
+    "tenzinkals11@gmail.com" : { url: "work.pecha.tools/glyph_annotation_gb/?session=tenzinkals11@gmail.com", department: "ocr" },
+    "wpema76@gmail.com" : { url: "work.pecha.tools/glyph_annotation_gb/?session=wpema76@gmail.com", department: "ocr" },
+
 }
 
 export const mapping = new Map(Object.entries(mappingObj));
