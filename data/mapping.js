1d3620d55ae6dc243b9d7843ba95d6bd163b8655
Tashi Tsering <43548581+ta4tsering@users.noreply.github.com>
2023-08-14 12:22:06 +0530

    added line to text mappings

--- data/mapping.js
+++ data/mapping.js
@@ -19,15 +19,26 @@ const mappingObj =
     "jamperthupten@gmail.com" : { url: "work.pecha.tools/glyph_ann_review_gb/?session=jamperthupten@gmail.com", department: "ocr"},
 
 
-    "dhephala@gmail.com" : { url: "work.pecha.tools/ls_perig_review/?session=dhephala@gmail.com", department: "ocr"},
-
-    
-    "dhondup995@gmail.com" : { url: "work.pecha.tools/line_to_text_ga/?session=dhondup995@gmail.com", department: "ocr"},
+    "ngawangsamten1989@gmail.com" : { url: "work.pecha.tools/line_to_text_ga/?session=ngawangsamten1989@gmail.com", department: "ocr"},
+    "kungachoden1983@gmail.com" : { url: "work.pecha.tools/line_to_text_ga/?session=kungachoden1983@gmail.com", department: "ocr"},
+    "sonamgyatso3767@gmail.com" : { url: "work.pecha.tools/line_to_text_ga/?session=sonamgyatso3767@gmail.com", department: "ocr"},
+    "tensam51@gmail.com" : { url: "work.pecha.tools/line_to_text_ga/?session=tensam51@gmail.com", department: "ocr"},
+    "dhephala@gmail.com" : { url: "work.pecha.tools/line_to_text_ga/?session=dhephala@gmail.com", department: "ocr"},
+    "kunchokd787@gmail.com" : { url: "work.pecha.tools/line_to_text_ga/?session=kunchokd787@gmail.com", department: "ocr"},
     "tthegchok1985@gmail.com" : { url: "work.pecha.tools/line_to_text_ga/?session=tthegchok1985@gmail.com", department: "ocr"},
-    
+
+
     "tenzintherchen154@gmail.com" : { url: "work.pecha.tools/line_to_text_gb/?session=tenzintherchen154@gmail.com", department: "ocr"},
-    "kunchokd787@gmail.com" : { url: "work.pecha.tools/line_to_text_gb/?session=kunchokd787@gmail.com", department: "ocr"},
-    "shugtso@monlam.ai" : { url: "work.pecha.tools/line_to_text_gb/?session=shugtso@monlam.ai", department: "ocr" },
+    "dhondup995@gmail.com" : { url: "work.pecha.tools/line_to_text_gb/?session=dhondup995@gmail.com", department: "ocr"},
+    "dhekharwork123@gmail.com" : { url: "work.pecha.tools/line_to_text_gb/?session=dhekharwork123@gmail.com", department: "ocr"},
+    "khedup62@gmail.com" : { url: "work.pecha.tools/line_to_text_gb/?session=khedup62@gmail.com", department: "ocr"},
+    "thubsamtharchin1982@gmail.com" : { url: "work.pecha.tools/line_to_text_gb/?session=thubsamtharchin1982@gmail.com", department: "ocr"},
+    "nordhentsering57@gmail.com" : { url: "work.pecha.tools/line_to_text_gb/?session=nordhentsering57@gmail.com", department: "ocr"},
+
+
+    "Kungalhundup1959@gamil.com" : { url: "work.pecha.tools/ltt_review_ga/?session=Kungalhundup1959@gamil.com", department: "ocr"},
+    "pemasungchuk@gmail.com" : { url: "work.pecha.tools/ltt_review_gb/?session=pemasungchuk@gmail.com", department: "ocr"},
+    
 }
 
 export const mapping = new Map(Object.entries(mappingObj));
